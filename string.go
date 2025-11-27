532a88b9a2fc047ec3025d0bde5866281b2bc1d9
Andika Rafon Sinuhaji <andika.rafon.senohaji@gmail.com>
2025-11-27 10:33:08 +0700

    Variable

---
+++ string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Andika")
+	fmt.Println(len("And ika"))
+	fmt.Println("Andika Rafon")
+	fmt.Println("Andika Rafon"[0])
+	fmt.Println("Andika Rafon Sinuhaji")
+}
